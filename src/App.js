6dd8f2bb34b4b923bec878a86e959356b9546768
alex.wade13@gmail.com <alex@tradewindmarkets.com>
2018-06-01 14:59:36 -0400

    completed header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './pics/nyt-logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="App">
+          <header className="App-header">
+            <div className="a-href-container" >
+              <a href="https://www.nytimes.com/" className="a-href">
+                <img src={logo} className="App-logo" alt="logo" />
+              </a>
+            </div>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
       </div>
     );
   }
